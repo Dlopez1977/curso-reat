11b771f7c326c020b1ed488df53a71fef8af62aa
Dlopez77 <myemail@example.com>
2023-07-08 03:56:05 -0400

    cursoReact

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+ const Boton = () => {
+    <button>   Twitter Card</button>
+}
+
+export default Boton
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import ReactDOM from 'react-dom/client'
+import Boton from "./App";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')) 
+root.render(
+  <Fragment>
+    <Boton/>
+    
+  </Fragment>,
+)
